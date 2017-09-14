62975e23b30c2aa032d448b46c941a632185a616
JLD T <jylland.teodosio@7thmedia.com>
2017-09-14 19:20:39 +0800

    Added new rates page

--- wp-content/themes/airtaxi/js/rates.js
+++ wp-content/themes/airtaxi/js/rates.js
@@ -1,5 +1,64 @@
 jQuery(function( $ ){
-    $('#airport-transfer').DataTable({
-        ordering:  false
+    
+    $(document).ready(function() {
+    
+        $('#airport-transfer').DataTable({
+            ordering :  false,
+            bInfo : false,
+            iDisplayLength : -1,
+            columnDefs : [
+                {
+                    targets : [ 1 ],
+                    className : 'dt-center',
+                    searchable : false
+                },
+                {
+                    targets : [ 2 ],
+                    searchable : false,
+                    className : 'dt-center'
+                },
+                {
+                    targets : [ 3 ],
+                    searchable : false,
+                    className : 'dt-center'
+                },
+                {
+                    targets : [ 4 ],
+                    searchable : false,
+                    className : 'dt-center'
+                }
+            ]
+        });
+    });
+    
+    $('.tabs-wrap .tab-links a').on('click', function(e)  {
+        var classname = $(this).attr('class');
+        var currentAttrValue = $(this).attr('href');
+        var tabselect = $('.'+classname+'-link');
+        
+        // Show/Hide Tabs
+        $('.tabs-wrap ' + currentAttrValue).fadeIn(400).siblings().hide();
+
+        // Change/remove current tab to active
+        tabselect.addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+    
+    $('.aerial-tours').slick({
+        dots: false,
+        arrows: true,
+        infinite: false,
+        rows: 2,
+        slidesPerRow: 4,
+        speed: 300,
+        slidesToShow: -1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: "unslick"
+            }
+        ]
     });
 });
--- wp-content/themes/airtaxi/part/part-rates-tabs.php
+++ wp-content/themes/airtaxi/part/part-rates-tabs.php
@@ -16,84 +16,147 @@
             <div class="tab-links-box">
                 <div class="tab-links-wrap">
                     <ul>
-                        <li class="tab-links">
-                            <h2><a href="#tab1"><?php echo get_field('airport_transfers_tab_name'); ?></a></h2>
+                        <li class="tab-links tab1-link active">
+                            <h2><a href="#tab1" class="tab1"><?php echo get_field('airport_transfers_tab_name'); ?></a></h2>
                         </li>
-                        <li class="tab-links">
-                            <h2><a href="#tab2"><?php echo get_field('aerial_tours_tab_name'); ?></a></h2>
+                        <li class="tab-links tab2-link">
+                            <h2><a href="#tab2" class="tab2"><?php echo get_field('aerial_tours_tab_name'); ?></a></h2>
                         </li>
-                        <li class="tab-links">
-                            <h2><a href="#tab3"><?php echo get_field('day_trips_tab_name'); ?></a></h2>
+                        <li class="tab-links tab3-link">
+                            <h2><a href="#tab3" class="tab3"><?php echo get_field('day_trips_tab_name'); ?></a></h2>
                         </li>
                     </ul>
                 </div>
             </div>
             <div class="tab-content">
-                <div id="1" class="tab active">
-                    <table id="airport-transfer">
-                        <thead>
-                            <tr>
-                                <th>Location</th>
-                                <th>Flight Time</th>
-                                <?php 
-                                $aircraft_capacity = array();
+                <div id="tab1" class="tab active">
+                    <div class="airport-transfer-wrap">
+                        <table id="airport-transfer" class="airport-transfer">
+                            <thead>
+                                <tr>
+                                    <th>Location</th>
+                                    <th>Location Type</th>
+                                    <th>Flight Time</th>
+                                    <?php 
+                                    $aircraft_capacity = array();
+
+                                    if(have_rows('airport_transfers_aircrafts')):
+                                    while(have_rows('airport_transfers_aircrafts')): the_row(); 
+
+                                        if(get_sub_field('aircraft')): 
+                                        $post = get_sub_field('aircraft');
+                                        setup_postdata($post);
+
+                                        $capacity = get_field('capacity');
+                                        
+                                        //* Display user icons depending on the aircraft capacity
+                                        ?>
+                                        <th>
+                                            <?php the_title(); ?>
+                                            <br/>
+                                            <?php for($i = 0; $i < $capacity; $i++): ?>
+                                                <i class="fa fa-user"></i>
+                                                <?php 
+                                                //* If user icon displayed is half the capacity, add spacing
+                                                if($i+1 == ($capacity/2)): ?>
+                                                    &nbsp;&nbsp;
+                                                <?php endif; ?>
+
+                                            <?php endfor; ?>
+                                        </th>
+
+                                        <?php wp_reset_postdata(); endif; ?>
+                                    <?php endwhile; endif;?>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if(have_rows('airport_transfers')): 
+                                while(have_rows('airport_transfers')): the_row();
                                 
-                                if(have_rows('airport_transfers_aircrafts')):
-                                while(have_rows('airport_transfers_aircrafts')): the_row(); 
-                                    
-                                    if(get_sub_field('aircraft')): 
-                                    $post = get_sub_field('aircraft');
-                                    setup_postdata($post);
-                                    
-                                    array_push($aircraft_capacity, get_field('capacity'));
-                                    ?>
-                                    
-                                    <th><?php the_title(); ?></th>
-                                    
-                                    <?php wp_reset_postdata(); endif; ?>
-                                <?php endwhile; endif;?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if(have_rows('airport_transfers')): 
-                            $header_displayed = 0;
-                            
-                            while(have_rows('airport_transfers')): the_row();
-                            $term = get_sub_field('location');
-                            
-                            //* If parent term is the same, display a parent term header
-                            if( $term->parent && ($header_displayed != $term->parent) ):
-                            $header_displayed = $term->parent;
-                            ?>
-                            <tr class="heading">
-                                <th><?php echo get_term($term->parent, 'locations')->name; ?></th>
-                                <th></th>
-                                <?php foreach($aircraft_capacity as $capacity): ?>
-                                <th><?php echo $capacity; ?></th>
-                                <?php endforeach;?>
-                            </tr>
-                            <?php endif; ?>
-                            
-                            <tr>
-                                <td><?php echo $term->name; ?></td>
-                                <td><?php echo get_sub_field('flight_time'); ?></td>
-                                <?php 
-                                if(have_rows('aircraft_rate')): 
-                                while(have_rows('aircraft_rate')): the_row(); ?>
-                               
-                                <td>P <?php echo get_sub_field('rate'); ?></td>
-                                
+                                //* Get term to get location (term->name) and location type (term->parent)
+                                $term = get_sub_field('location');
+                                ?>
+
+                                <tr>
+                                    <td><?php echo $term->name; ?></td>
+                                    <td><?php echo get_term($term->parent, 'locations')->name; ?></td>
+                                    <td><?php echo get_sub_field('flight_time'); ?></td>
+                                    <?php 
+                                    if(have_rows('aircraft_rate')): 
+                                    while(have_rows('aircraft_rate')): the_row(); ?>
+
+                                    <td>P <?php echo get_sub_field('rate'); ?></td>
+
+                                    <?php endwhile; endif; ?>
+
+                                </tr>
                                 <?php endwhile; endif; ?>
-                            
-                            </tr>
-                            <?php endwhile; endif; ?>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 
-                <div id="2" class="tab"></div>
+                <div id="tab2" class="tab">
+                    <div class="aerial-tours-wrap">
+                        <div class="aerial-tours">
+                            <?php 
+                            if(have_rows('aerial_tours')):
+                            while(have_rows('aerial_tours')): the_row(); ?>
+
+                            <div class="rates-box">
+
+                                <div class="rates-box-row">
+                                    <div class="rates-box-header">
+                                        <?php echo get_sub_field('tour_name'); ?>
+                                    </div>
+                                </div>
+                                <div class="rates-box-row">
+                                    <div class="rates-box-title">
+                                        <?php echo get_sub_field('tour_highlights'); ?>
+                                    </div>
+                                </div>
+                                <div class="rates-box-duration">
+                                    <?php echo get_sub_field('duration'); ?>
+                                </div>
+
+                                <table class="rates-box-list">
+                                <?php
+                                if( have_rows('aircraft_rates') ):
+                                while ( have_rows('aircraft_rates') ) : the_row();
+
+                                    if(get_sub_field('aircraft')):
+                                    $post = get_sub_field('aircraft');
+                                    $rate = get_sub_field('rate');
+                                    setup_postdata($post);
+
+                                    ?>
+                                    <tr class="rates-box-item">
+                                        <td>
+                                            <div class="item-capacity">
+                                                <?php echo get_field('capacity'); ?> PAX
+                                            </div>
+                                            <div class="item-aircraft">
+                                                <?php echo get_field('model'); ?>
+                                            </div>
+                                        </td>
+
+                                        <td>
+                                            <div class="item-price">
+                                                &#8369; <?php echo $rate; ?>
+                                            </div>
+                                       </td>
+                                   </tr>
+                                    <?php wp_reset_postdata(); endif; ?>
+                                <?php endwhile; endif; ?>
+                                </table>
+                            </div>
+
+                            <?php endwhile; endif; ?>
+                        </div>
+                    </div>
+                </div>
                 
-                <div id="3" class="tab"></div>
+                <div id="tab3" class="tab"></div>
             </div>
         </div>
     </div>
